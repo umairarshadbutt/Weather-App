aa9932df21844f3d2399ab79314efde2174520d5
Umair Arshad <Umair6622@gmail.com>
2021-03-23 01:02:11 +0500

    Update the layout with API

--- src/index.js
+++ src/index.js
@@ -5,7 +5,42 @@ function component() {
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    
+    const searchbox = document.querySelector('.search-box');
+    searchbox.addEventListener('keypress', enterQuery);
+    
+    function enterQuery(evt) {
+      if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+      }
+    }
+    
+    function getResults (query) {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=ab8e5103c464904aa015d26efbdb8353`)
+        .then(weather => {
+          return weather.json();
+        }).then(results);
+    }
+    
+    function results (weather) {
+      let city = document.querySelector('.location .city');
+      city.innerText = `${weather.name}, ${weather.sys.country}`;
+    
+      let now = new Date();
+      let date = document.querySelector('.location .date');
+     
+    
+      let temprature = document.querySelector('.current .temprature');
+      temprature.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+    
+      let weather_el = document.querySelector('.current .weather');
+      weather_el.innerText = weather.weather[0].main;
+    
+      let hilow = document.querySelector('.hi-low');
+      hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+    }
+    
+    
     return element;
   }
   
