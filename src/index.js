8c60bb8a02e07f28926e9bec8aaff0c86d661376
Umair Arshad <Umair6622@gmail.com>
2021-03-23 04:24:52 +0500

    Fix the ESlint isses

--- src/index.js
+++ src/index.js
@@ -1,57 +1,54 @@
 import _ from 'lodash';
 import './style.css';
+
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    
-    const searchbox = document.querySelector('.search-box');
-    searchbox.addEventListener('keypress', enterQuery);
-    
-    function enterQuery(evt) {
-      if (evt.keyCode == 13) {
-        getResults(searchbox.value);
-      }
-    }
-    
-    function getResults (query) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=ab8e5103c464904aa015d26efbdb8353`)
-        .then(weather => {
-          return weather.json();
-        }).then(results);
-    }
-    
-    function results (weather) {
-      let city = document.querySelector('.location .city');
-      city.innerText = `${weather.name}, ${weather.sys.country}`;
-    
-      let now = new Date();
-      let date = document.querySelector('.location .date');
-      date.innerText = dates(now);
-    
-      let temprature = document.querySelector('.current .temprature');
-      temprature.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-    
-      let weather_el = document.querySelector('.current .weather');
-      weather_el.innerText = weather.weather[0].main;
-    
-      let hilow = document.querySelector('.hi-low');
-      hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
-    }
-    
-    function dates (d) {
-      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-    
-      return `${day} ${date} ${month} ${year}`;
-    }
-    return element;
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  const searchbox = document.querySelector('.search-box');
+  function dates(d) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    const day = days[d.getDay()];
+    const date = d.getDate();
+    const month = months[d.getMonth()];
+    const year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
   }
-  
-  document.body.appendChild(component());
+  function results(weather) {
+    const city = document.querySelector('.location .city');
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    const now = new Date();
+    const date = document.querySelector('.location .date');
+    date.innerText = dates(now);
+
+    const temprature = document.querySelector('.current .temprature');
+    temprature.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+    const weatherElement = document.querySelector('.current .weather');
+    weatherElement.innerText = weather.weather[0].main;
+
+    const hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+  }
+  function getResults(query) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=ab8e5103c464904aa015d26efbdb8353`)
+      .then(weather => weather.json()).then(results);
+  }
+
+  function enterQuery(evt) {
+    if (evt.keyCode === 13) {
+      getResults(searchbox.value);
+    }
+  }
+  searchbox.addEventListener('keypress', enterQuery);
+
+
+  return element;
+}
+
+document.body.appendChild(component());
